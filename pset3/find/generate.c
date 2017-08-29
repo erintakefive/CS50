7e6db6154c80d8ad52cea7ec004c19d81402197c
Erin <erintakefive@gmail.com>
2017-08-29 14:55:18 +0100

    Create generate.c

---
+++ pset3/find/generate.c
@@ -0,0 +1,52 @@
+/**
+ * generate.c
+ *
+ * Generates pseudorandom numbers in [0,MAX), one per line.
+ *
+ * Usage: generate n [s]
+ *
+ * where n is number of pseudorandom numbers to print
+ * and s is an optional seed
+ */
+ 
+#define _XOPEN_SOURCE
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// upper limit on range of integers that can be generated
+#define LIMIT 65536
+
+int main(int argc, string argv[])
+{
+    // if argument count is not 2 or 3, exit
+    if (argc != 2 && argc != 3)
+    {
+        printf("Usage: ./generate n [s]\n");
+        return 1;
+    }
+
+    // cast string argv[1] to an integer (n)
+    int n = atoi(argv[1]);
+
+    // detect and utilize seed, if provided
+    if (argc == 3)
+    {
+        srand48((long) atoi(argv[2]));
+    }
+    else
+    {
+        srand48((long) time(NULL));
+    }
+
+    // print amount of random numbers provided by user
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", (int) (drand48() * LIMIT));
+    }
+
+    // success
+    return 0;
+}
